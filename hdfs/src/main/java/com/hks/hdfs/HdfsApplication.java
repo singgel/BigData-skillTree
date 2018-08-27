0741e38c69f24c9d7ffcdb52976cbe9a497f54d1
heks <heks@yiche.com>
2018-08-27 11:36:14 +0800

    Initial commit

---
+++ hdfs/src/main/java/com/hks/hdfs/HdfsApplication.java
@@ -0,0 +1,13 @@
+package com.hks.hdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdfsApplication.class, args);
+	}
+	
+}
---
+++ hdfs/src/test/java/com/hks/hdfs/HdfsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hks.hdfs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HdfsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
