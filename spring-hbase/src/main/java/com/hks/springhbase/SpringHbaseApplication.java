9edbac5fefd10aa6030ad90ea35d3d2ab00d81df
heks <heks@yiche.com>
2018-11-01 16:49:21 +0800

    hbase

---
+++ spring-hbase/src/main/java/com/hks/springhbase/SpringHbaseApplication.java
@@ -0,0 +1,12 @@
+package com.hks.springhbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHbaseApplication.class, args);
+	}
+}
---
+++ spring-hbase/src/main/java/com/hks/springhbase/Test.java
@@ -0,0 +1,11 @@
+package com.hks.springhbase;
+
+/**
+ * @Author: hekuangsheng
+ * @Date: 2018/11/1
+ */
+public class Test {
+
+
+
+}
