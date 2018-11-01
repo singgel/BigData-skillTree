464c46dd4482915f4918bedabbd0e96a72d7a752
heks <heks@yiche.com>
2018-11-01 17:17:45 +0800

    我准备暂时搁置，妈的这块的知识太扯了

---
+++ word-count/src/main/java/com/hks/wordcount/WordCountApplication.java
@@ -0,0 +1,12 @@
+package com.hks.wordcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordCountApplication.class, args);
+	}
+}
