7fea22319f78dd5c9999607571f476acb2036dbc
heks <heks@yiche.com>
2018-12-03 15:50:55 +0800

    hive init

--- spring-boot-hive/src/main/java/com/hks/hive/SpringBootHiveApplication.java
+++ spring-boot-hive/src/main/java/com/hks/hive/SpringBootHiveApplication.java
@@ -1,12 +1,12 @@
-package com.hks.wordcount;
+package com.hks.hive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WordCountApplication {
+public class SpringBootHiveApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WordCountApplication.class, args);
+		SpringApplication.run(SpringBootHiveApplication.class, args);
 	}
 }
--- spring-boot-hive/src/test/java/com/hks/hive/SpringBootHiveApplicationTests.java
+++ spring-boot-hive/src/test/java/com/hks/hive/SpringBootHiveApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hks.springhive;
+package com.hks.hive;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringHiveApplicationTests {
+public class SpringBootHiveApplicationTests {
 
 	@Test
 	public void contextLoads() {
