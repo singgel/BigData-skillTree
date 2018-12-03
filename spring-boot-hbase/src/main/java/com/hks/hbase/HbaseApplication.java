1b7196f36144217f18f290f3c9df25e3358dbc75
heks <heks@yiche.com>
2018-12-03 15:47:33 +0800

    纯手工编写的java版jdbc模式的api

---
+++ spring-boot-hbase/src/main/java/com/hks/hbase/HbaseApplication.java
@@ -0,0 +1,17 @@
+package com.hks.hbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+@SpringBootApplication
+public class HbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbaseApplication.class, args);
+    }
+
+}
--- spring-boot-hbase/src/test/java/com/hks/hbase/HbaseApplicationTests.java
+++ spring-boot-hbase/src/test/java/com/hks/hbase/HbaseApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hks.wordcount;
+package com.hks.hbase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WordCountApplicationTests {
+public class HbaseApplicationTests {
 
 	@Test
 	public void contextLoads() {
